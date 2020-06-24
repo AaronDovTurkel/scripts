444f8877320ea8927e2f20080674d6b6704ef2f8
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2020-06-24 13:52:18 -0400

    added amount od char length param

--- random-word.sh
+++ random-word.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+if [ "$1" != "" ]; then
+
+GREPPED_WORDS=($(grep -E "^\w{1,$1}$" /usr/share/dict/words))
+
+GREPPED_LENGTH=${#GREPPED_WORDS[@]}
+
+GREPPED_RANDOM_INDEX=$(jot -r 1 0 $GREPPED_LENGTH)
+
+GREPPED_RANDOM_WORD=${GREPPED_WORDS[$GREPPED_RANDOM_INDEX]}
+
+echo $GREPPED_RANDOM_WORD
+
+else
+
 WORDS=($(cat /usr/share/dict/words))
 
 LENGTH=${#WORDS[@]}
 
 RANDOM_INDEX=$(jot -r 1  0 $LENGTH)
 
-echo ${WORDS[$RANDOM_INDEX]}
+RANDOM_WORD=${WORDS[$RANDOM_INDEX]}
+
+echo $RANDOM_WORD
+
+fi
 
